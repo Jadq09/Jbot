4859ccb6b872473fe8b0da9066c0636fef535bfb
Jadq09 <jorgeadanduran@hotmail.com>
2021-12-24 23:57:51 -0700

    Se ha modificado el código del archivos messageUpdate.js que se encuentra en la carpeta Eventos. El código impide que mande un log si el autor es Jbot, esto debido a un error en el sistema de Jbot.

--- Eventos/messageUpdate.js
+++ Eventos/messageUpdate.js
@@ -4,6 +4,9 @@ const logs = new db.crearDB("logs")
 
 module.exports = async (client, oldMessage, newMessage) => {
 
+  if(oldMessage.author.id === '881445730018000937') return;
+  if(newMessage.author.id === '881445730018000937') return;
+
   const embed = new Discord.MessageEmbed()
   .setTitle("Mensaje fachero editado!")
   .setDescription(`Antiguo mensaje: **${oldMessage}**\nNuevo mensaje: **${newMessage}**\nAuthor: **${newMessage.author.tag}**\nCanal: **${oldMessage.channel}**\nID: **${newMessage.id}**`)
